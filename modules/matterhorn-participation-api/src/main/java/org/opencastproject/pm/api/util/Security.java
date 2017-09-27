47ed8c06cc3aea85127ffdebeeed9de456994d2f
James S Perrin <james.s.perrin@manchester.ac.uk>
2017-09-27 09:44:05 +0100

    MAT-255, use stdout as log imposes additional formating OC seems to need the common (for Crypt) bundle explicitly embedded (reverted from commit ef21226367375ebc4e06600a3ee3e81318eb638f)

--- modules/matterhorn-participation-api/src/main/java/org/opencastproject/pm/api/util/Security.java
+++ modules/matterhorn-participation-api/src/main/java/org/opencastproject/pm/api/util/Security.java
@@ -23,9 +23,13 @@ package org.opencastproject.pm.api.util;
 
 import org.opencastproject.util.Crypt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.security.Key;
 
 public final class Security {
+  private static final Logger logger = LoggerFactory.getLogger(Security.class);
 
   private Security() {
   }
@@ -39,13 +43,11 @@ public final class Security {
    *          the commandline arguments
    */
   public static void main(String[] args) {
-//CHECKSTYLE:OFF
     if (args.length == 1) {
-      System.out.println(Crypt.encrypt(Security.TEACHER_EMAIL_KEY, args[0]));
+      logger.info(Crypt.encrypt(Security.TEACHER_EMAIL_KEY, args[0]));
     } else {
-      System.err.println("Usage: java -jar matterhorn-participation-api.jar <email address>");
+      logger.info("Usage: java -jar matterhorn-participation-api.jar <email address>");
     }
-//CHECKSTYLE:ON
   }
 
 }
