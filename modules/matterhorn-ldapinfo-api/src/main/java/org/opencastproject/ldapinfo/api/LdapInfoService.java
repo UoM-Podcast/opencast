d5d1db4a6c95038b9511fc20e4df4f70d720e8b0
Tobias Schiebeck <ts23gm@gmail.com>
2019-11-04 13:24:11 +0000

    MAT-336 LDAP Information Service REST endpoint to provide UoM SpotId and LDAP group membership information to the podcast tools

---
+++ modules/matterhorn-ldapinfo-api/src/main/java/org/opencastproject/ldapinfo/api/LdapInfoService.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ *
+ * The Apereo Foundation licenses this file to you under the Educational
+ * Community License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at:
+ *
+ *   http://opensource.org/licenses/ecl2.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+
+package org.opencastproject.ldapinfo.api;
+
+/**
+ * Api for the LDAP Information Service
+ * 
+ * An Opencast Service to query a local LDAP server to get limited user information from a username.
+ */
+public interface LdapInfoService {
+
+  /**
+   * Get the spotId for the given user
+   * @param username
+   *          the username to look up
+   * @return The spotId of the user
+   * @throws org.opencastproject.ldapinfo.api.LdapInfoException
+   */
+  String getSpotId(String username) throws LdapInfoException;
+
+  /**
+   * Check the group membership of a given user.
+   *
+   * @param username
+   *          the username to look up
+   * @param group
+   *          the LDAP group to be tested for the user
+   * @return true   if the user is a member of the group
+   *         false  otherwise
+   * @throws org.opencastproject.ldapinfo.api.LdapInfoException
+   */
+  boolean isMemberOf(String username, String group) throws LdapInfoException;
+}
