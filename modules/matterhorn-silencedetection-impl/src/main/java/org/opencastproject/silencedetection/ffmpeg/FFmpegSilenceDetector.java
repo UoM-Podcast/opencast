a179d1aa52036809a8936ac39ff59954ff079416
Paul Pettit <paul.pettit@manchester.ac.uk>
2020-11-06 16:16:54 +0000

    MAT-365: fix silence detector for latest ffmpeg
    mostly, this is just changing the silence.mp4 to the one
    in current upstream develop, the same failures occur
    in upstream with the old silence.mp4 so there is a bug
    somewhere (also in upstream) though it doesn't look
    important.

--- modules/matterhorn-silencedetection-impl/src/main/java/org/opencastproject/silencedetection/ffmpeg/FFmpegSilenceDetector.java
+++ modules/matterhorn-silencedetection-impl/src/main/java/org/opencastproject/silencedetection/ffmpeg/FFmpegSilenceDetector.java
@@ -152,7 +152,8 @@ public class FFmpegSilenceDetector {
     DecimalFormat decimalFmt = new DecimalFormat("0.000", new DecimalFormatSymbols(Locale.US));
     String minSilenceLengthInSeconds = decimalFmt.format((double) minSilenceLength / 1000.0);
     String filter = "silencedetect=noise=" + thresholdDB + ":duration=" + minSilenceLengthInSeconds;
-    String[] command = new String[] {binary, "-nostats", "-i", mediaPath, "-filter:a", filter, "-f", "null", "-"};
+    String[] command = new String[] {
+      binary, "-nostats", "-nostdin", "-i", filePath, "-vn", "-filter:a", filter, "-f", "null", "-"};
     String commandline = StringUtils.join(command, " ");
 
     logger.info("Running {}", commandline);
